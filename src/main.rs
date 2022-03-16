0473b3ecf59cddbfb85191d58c60be20783bf39f
Real-Musafir <alshahadath@gmail.com>
2022-03-16 20:56:11 +0600

    handling invalid number

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
