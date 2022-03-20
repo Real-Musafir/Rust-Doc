504deb4116c6049b0fb9620c163adc3f0e130b8a
Real-Musafir <alshahadath@gmail.com>
2022-03-20 12:42:19 +0600

    filter Upercase I started ine

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use std::fs::read_to_string;
 
 fn main() {
   let input: String = read_to_string("lines.txt").unwrap();
-  println!("{}", input);
+  for line in input.lines(){
+      if starts_with_capital_letter(line) {
+          println!("-{}", line);
+      }
+  }
+}
 
+#[allow(unused)]
+fn starts_with_capital_letter(s: &str)-> bool {
+    matches!(s.chars().next(), Some(c) if c.is_uppercase())
 }
