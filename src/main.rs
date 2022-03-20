0008628e7daefc8f72a5e4e3337e7a65b21c54c8
Real-Musafir <alshahadath@gmail.com>
2022-03-20 14:48:59 +0600

    push desirable lines in a vector and print

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use std::fs::read_to_string;
 
 fn main() {
   let input: String = read_to_string("lines.txt").unwrap();
+
+  let mut good_lines: Vec<&str> = vec![];
+
   for line in input.lines().filter(|I:&&str| starts_with_capital_letter(I)){
       if starts_with_capital_letter(line) {
-          println!("-{}", line);
+          good_lines.push(line);
       }
   }
+   dbg!(good_lines);
 }
 
 #[allow(unused)]
